d6f4bbeec2e614e69c3390f9160ed52a3a724a4e
Manvi Agarwal <142096691+manviag05@users.noreply.github.com>
2023-11-02 14:53:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,76 @@
-class DictionaryApp:
-    def __init__(self):
-        self.dictionary = {}
+import face_recognition
+import cv2
+import numpy as np
+import csv 
+from datetime import datetime
 
-    def create_dictionary(self):
-        filename = "dictionary.txt"
-        try:
-            with open(filename, "w") as file:
-                for word, meaning in self.dictionary.items():
-                    file.write(f"{word}: {meaning}\n")
-            print("Dictionary has been generated successfully.")
-        except IOError:
-            print("Error: Unable to generate the dictionary file.")
+video_capture = cv2.VideoCapture(0)
 
-    def insert_word_meaning(self):
-        word = input("Enter a word: ")
-        if word in self.dictionary :
-            print("Word already present in the dictionary.\nTry again with another word!")
-            return
-        meaning = input(f"Enter the meaning of '{word}': ")
-        self.dictionary[word] = meaning
-        print(f"'{word}' added to the dictionary.")
+# Load known faces
 
-    def search_word_meaning(self):
-        word = input("Enter a word to search: ")
-        meaning = self.dictionary.get(word, "Word not found in the dictionary.")
-        print(f"The meaning of '{word}'' is : '{meaning}'")
+alakh_image = face_recognition.load_image_file("Students/Alakh.jpg")
+alakh_encoding = face_recognition.face_encodings(alakh_image)[0]
 
-    def delete_word(self):
-        word = input("Enter a word to delete: ")
-        if word in self.dictionary:
-            del self.dictionary[word]
-            print(f"'{word}' deleted from the dictionary.")
-        else:
-            print(f"'{word}' not found in the dictionary.")
+ishan_image = face_recognition.load_image_file("Students/Ishan.jpg")
+ishan_encoding = face_recognition.face_encodings(ishan_image)[0]
 
-    def menu(self):
-        while True:
-            print("\nDictionary Application Menu:")
-            print("1. Generate Dictionary")
-            print("2. Insert New Word-Meaning")
-            print("3. Search for a Word-Meaning")
-            print("4. Delete a Word")
-            print("5. Exit Application")
-            choice = input("Enter the function you wish to perform (1/2/3/4/5): ")
+varun_image = face_recognition.load_image_file("Students/Varun.jpg")
+varun_encoding = face_recognition.face_encodings(varun_image)[0]
 
-            if choice == "1":
-                self.create_dictionary()
-            elif choice == "2":
-                self.insert_word_meaning()
-            elif choice == "3":
-                self.search_word_meaning()
-            elif choice == "4":
-                self.delete_word()
-            elif choice == "5":
-                break
-            else:
-                print("Invalid choice. Please try again.")
+known_face_encodings = [alakh_encoding , ishan_encoding , varun_encoding]
+known_face_names = ["Alakh" , "Ishan" , "Varun"]
 
-if __name__ == "__main__":
-    tool = DictionaryApp()
-    tool.menu()
+# List of expected students
+
+students = known_face_names.copy()
+
+face_locations = []
+face_encodings = []
+
+# Get the current date and time
+
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+
+f = open(f"{current_date}.csv" , "w+" , newline="")
+lnwriter = csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame ,(0,0) , fx=0.25 , fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame , cv2.COLOR_BGR2RGB) 
+    
+    # Recognise faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame , face_locations)
+    
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings,face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings,face_encoding)
+        best_match_index = np.argmin(face_distance)
+        
+        if(matches[best_match_index]):
+            name = known_face_names[best_match_index]
+            
+            # Add the text if a person is present
+            if name in known_face_names:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                bottomLeftCornerOfText = (10,100)
+                fontScale = 1.5
+                fontColor = (255,0,0)
+                thickness = 3
+                lineType = 2
+                cv2.putText(frame, name + " Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+                
+                if name in students:
+                    students.remove(name)
+                    current_time = now.strftime("%H:%M:%S")
+                    lnwriter.writerow([name,current_time])
+    
+    cv2.imshow("Camera" , frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
